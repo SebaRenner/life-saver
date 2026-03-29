2468c5b3fff12ab7f86cc1c64f4452961b00ae2f
SebaRenner <sebastian.renner@gmx.ch>
2026-03-29 22:22:07 +0200

    feat: added feature husks and setup routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { EmergencyInfo } from './features/emergency-info/emergency-info';
+import { Login } from './features/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: EmergencyInfo },
+  { path: 'login', component: Login },
+  { path: 'edit', component: EmergencyInfo },
+  { path: '**', component: EmergencyInfo },
+];
---
+++ src/app/features/emergency-info/emergency-info.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emergency-info',
+  imports: [],
+  templateUrl: './emergency-info.html',
+  styleUrl: './emergency-info.scss',
+})
+export class EmergencyInfo {}
