d370f29c702da0c649a5ddb375d36299f9925c38
SebaRenner <sebastian.renner@gmx.ch>
2026-03-29 23:15:03 +0200

    feat: implemented blood type selector component

---
+++ src/app/components/blood-type/blood-type.component.ts
@@ -0,0 +1,52 @@
+import { Component, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { BloodType } from '../../models/blood-type.model';
+
+@Component({
+  selector: 'app-blood-type',
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule
+  ],
+  templateUrl: './blood-type.component.html',
+  styleUrl: './blood-type.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => BloodTypeComponent),
+      multi: true,
+    }
+  ],
+})
+export class BloodTypeComponent implements ControlValueAccessor {
+  readonly options = Object.values(BloodType);
+
+  value: BloodType | null = null;
+  isDisabled = false;
+
+  // =============================================
+  // ControlValueAccessor
+  // =============================================
+
+  onChange: (value: BloodType | null) => void = () => {};
+  onTouched: () => void = () => {};
+
+  writeValue(value: BloodType | null): void {
+    this.value = value ?? null;
+  }
+ 
+  registerOnChange(fn: (value: BloodType | null) => void): void {
+      this.onChange = fn;
+  }
+ 
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+  
+  setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+}
--- src/app/features/emergency-info/emergency-info.ts
+++ src/app/features/emergency-info/emergency-info.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BloodTypeComponent } from '../../components/blood-type/blood-type.component';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-emergency-info',
-  imports: [],
+  imports: [ReactiveFormsModule, BloodTypeComponent, MatButton],
   templateUrl: './emergency-info.html',
   styleUrl: './emergency-info.scss',
 })
-export class EmergencyInfo {}
+export class EmergencyInfo {
+  private readonly fb = inject(FormBuilder);
+
+  form = this.fb.group({
+    bloodType: [null],
+  });
+
+  onSubmit(): void {
+    console.log(this.form.value);
+  }
+}
