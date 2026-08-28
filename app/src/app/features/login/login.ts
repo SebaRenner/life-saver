fc8369d62f530a486029e80f2f97ca88991ccd75
SebaRenner <sebastian.renner@gmx.ch>
2026-08-28 22:48:48 +0200

    feat(app): implemented basic login component

--- app/src/app/features/login/login.ts
+++ app/src/app/features/login/login.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+  readonly loginForm: FormGroup;
+
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+
+  constructor(fb: FormBuilder) {
+    this.loginForm = fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      this.authService.login(email, password).subscribe((response) => {
+        // Store ID in Session Store
+
+        this.router.navigate(['']);
+      });
+    }
+  }
+}
