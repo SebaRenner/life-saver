daf658a62fd4d4f7da3d06186fb813cb61f2cccf
SebaRenner <sebastian.renner@gmx.ch>
2026-08-29 23:58:37 +0200

    feat(app): added basic fetch logic to view feature

--- app/src/app/features/emergency-info-view/emergency-info-view.ts
+++ app/src/app/features/emergency-info-view/emergency-info-view.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserProfileService } from '../../services/user-profile.service';
+import { ActivatedRoute } from '@angular/router';
+import { catchError, map, of, startWith, switchMap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { UserProfile } from '../../models/user-profile.model';
+import { SpinnerComponent } from '../../components/spinner/spinner.component';
+
+type ProfileState =
+  | { status: 'loading' }
+  | { status: 'loaded'; data: UserProfile }
+  | { status: 'not_found' };
 
 @Component({
-  imports: [],
+  imports: [AsyncPipe, SpinnerComponent],
   selector: 'app-emergency-info-view',
   styleUrl: './emergency-info-view.scss',
   templateUrl: './emergency-info-view.html',
 })
-export class EmergencyInfoView {}
+export class EmergencyInfoView {
+  private readonly userProfileService = inject(UserProfileService);
+  private readonly route = inject(ActivatedRoute);
+
+  profileState$ = this.route.paramMap.pipe(
+    switchMap((params) => {
+      const userId = params.get('id');
+      if (!userId) return of({ status: 'not_found' } as ProfileState);
+
+      return this.userProfileService.getById(userId).pipe(
+        map((data) => ({ status: 'loaded', data }) as ProfileState),
+        catchError(() => of({ status: 'not_found' } as ProfileState)),
+      );
+    }),
+    startWith({ status: 'loading' } as ProfileState),
+  );
+}
