154388c1203155fba355342882a70031996a1559
SebaRenner <sebastian.renner@gmx.ch>
2026-08-31 16:23:59 +0200

    feat: added app service plan and linux web app

---
+++ terraform/web_app.tf
@@ -0,0 +1,20 @@
+resource "azurerm_service_plan" "asp" {
+  name                = "asp-life-saver"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = local.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "api" {
+  name                = "app-life-saver"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = local.location
+  service_plan_id     = azurerm_service_plan.asp.id
+
+  site_config {
+    application_stack {
+      dotnet_version = "10.0"
+    }
+  }
+}
